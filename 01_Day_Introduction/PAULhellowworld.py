bcdb3ebe2be97021cb725a885f11ce3c5cad3777
Paul Piwowarski <pauldpiwowarski@gmail.com>
2025-12-13 14:55:29 -0500

    edited hellowWorldPaul

--- 01_Day_Introduction/PAULhellowworld.py
+++ 01_Day_Introduction/PAULhellowworld.py
@@ -2,3 +2,25 @@
 #code along side it
 #do exercises at the bottom
 #Run your notes
+
+print(5 + 8)
+print(8 - 5)
+print(6 * 4)
+print(9 / 4)
+print(3 ** 4) #exponential
+print(9 % 5)
+print(5 // 2) #floor division
+
+# Checking data types
+print(type(10))
+print(type(3.45))
+print(type(5 + 4j)) #complex
+#cannot do print(type(5i + 3))
+
+print(type('Abraham'))
+print(type(3 == 5)) #bool
+
+print(type([1, 5, 6])) #list (Ordered, mutable, allows duplicates)
+print(type((9.8, 23.3, 12.3))) #tuple (Ordered, immutable, allows duplicates)
+print(type({4, 6, 7})) #set (Unordered, mutable, no duplicates)
+print(type({'name' : 'paul'})) #dictionary (Key–value pairs, ordered, mutable, keys must be unique)
